81e04cb2fe1af0a32859eb62bacd68bd9093fb74
havlong <grishin_evgenii@inbox.ru>
2022-12-03 18:12:42 +0300

    Packaging implemented

--- src/main/java/magneticcode/boxback/dto/client/ProductDTO.java
+++ src/main/java/magneticcode/boxback/dto/client/ProductDTO.java
@@ -5,6 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record ProductDTO(@JsonProperty("productName") String name,
-                         @JsonProperty("productDimensions") List<ProductParamsDTO> productDimensions) {
+                         @JsonProperty("productDimentions") List<ProductParamsDTO> productDimensions) {
 
 }
